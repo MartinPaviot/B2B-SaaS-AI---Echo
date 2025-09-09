1a6f3208df98bf9000bb1abab2a68a5846d63c44
Martin Paviot <martin.paviot@outlook.com>
2025-09-09 22:55:20 +0200

    05: Error Tracking

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -4,12 +4,9 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import {useMutation, useQuery,} from "convex/react";
 import {api} from "@workspace/backend/_generated/api";
 import {Button} from "@workspace/ui/components/button";
-import { Sign } from "crypto";
-import { Organization } from "@clerk/nextjs/server";
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
-  const addUser = useMutation(api.users.add);
+   const addUser = useMutation(api.users.add);
 
   return (
       <div className="flex flex-col items-center justify-center min-h-svh">
@@ -17,9 +14,6 @@ export default function Page() {
         <UserButton />
         <OrganizationSwitcher hidePersonal />
         <Button onClick ={() => addUser()}>Add</Button>
-        <div className="max-w-sm w-full mx-auto">
-          {JSON.stringify(users, null, 2)}
-        </div>  
       </div>
   )
 }
--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -24,6 +24,8 @@ export const add = mutation ({
             throw new Error ("Missing organization");
         }
         
+        throw new Error("Tracking test");
+
         const userID = await ctx.db.insert ("users", {
            name: "Antonio",
         });
